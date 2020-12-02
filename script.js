04d753cdae7096b5a3075ff800fcdacc7fa56a53
Emily Spoldi <emily.l.spoldi@gmail.com>
2020-12-02 15:57:32 -0500

    added rows and columns for hour blocks

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 let date = moment().format(`dddd, MMMM Do YYYY`);
 $("#currentDay").text(date);
 
+//Create 9 rows of hour blocks
+for (let i = 0; i < 9; i++) {
+    $(".container").append(`<row id="${i}">`);
+    $(`#${i}`).append(`<div class="col-md-2">`);
+    $(`#${i}`).append(`<div class="col-md-auto">`);
+    $(`#${i}`).append(`<div class="col-md-2">`);
+}
